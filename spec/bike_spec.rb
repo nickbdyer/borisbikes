2fd76ee99b697acd2cc9777b22baeb40b89642c1
Nick Dyer <nbdyer@gmail.com>
2014-10-28 20:03:11 +0000

    Refactor

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,22 +2,21 @@ require 'bike'
 
 describe Bike do
 
+  let(:bike) { Bike.new }
+
   it 'should not be broken after we create it' do
-    the_bike = Bike.new
-    expect(the_bike).not_to be_broken
+    expect(bike).not_to be_broken
   end
 
   it 'should be able to break' do
-    bike = Bike.new
     bike.break!
     expect(bike).to be_broken
   end
 
   it 'should be able to get fixed' do
-    bike = Bike.new
     bike.break!
     bike.fix!
     expect(bike).not_to be_broken
   end
-  
+
 end
