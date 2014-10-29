d4dab9247404f18cbc01eabae11c5ba79c3890e1
Nick Dyer <nbdyer@gmail.com>
2014-10-29 16:30:26 +0000

    Refactor garage to use bikecontainer module

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,27 +1,11 @@
+require_relative 'bike_container'
+
 class Garage
 
-DEFAULT_CAPACITY = 50
+  include BikeContainer
 
   def initialize(options = {})
-    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-    @bikes = []
-  end
-
-  def bike_count
-    @bikes.count
-  end
-
-  def accept_bike(bike)
-    raise 'Garage is full' if full?
-    @bikes << bike
-  end
-
-  def release_bike(bike)
-    @bikes.delete(bike)
-  end
-
-  def full?
-    bike_count == @capacity
+    @capacity = options.fetch(:capacity, capacity)
   end
 
   def repair!(bike)
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -3,35 +3,12 @@ require 'garage'
 describe 'Garage' do
 
   let(:bike) {Bike.new}
-  let(:garage) {Garage.new}
+  let(:garage) {Garage.new(capacity: 200)}
 
-  def fill_garage(garage)
-    50.times { garage.accept_bike(bike) }
+  it "should allow setting a default capacity on initializing" do
+    expect(garage.capacity).to eq(200)
   end
-
-  it "should accept a bike" do
-    expect(garage.bike_count).to eq(0)
-    garage.accept_bike(bike)
-    expect(garage.bike_count).to eq(1) 
-  end
-
-  it "should release a bike" do
-    garage.accept_bike(bike)
-    garage.release_bike(bike)
-    expect(garage.bike_count).to eq(0)
-  end
-
-  it "should know when it is full" do
-    expect(garage).not_to be_full
-    fill_garage garage
-    expect(garage).to be_full
-  end
-
-  it "should not accept a bike when it is full" do
-    fill_garage garage
-    expect( lambda { garage.accept_bike(bike) }).to raise_error(RuntimeError)
-  end
-
+  
   it "should fix a bike" do
     bike.break!
     garage.repair!(bike)
