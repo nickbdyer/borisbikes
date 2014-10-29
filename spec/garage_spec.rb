80e836f99ac23be8ea11bfae3c4b0bca33ea9a6d
Nick Dyer <nbdyer@gmail.com>
2014-10-29 12:46:46 +0000

    Refactor

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -2,29 +2,30 @@ require 'garage'
 
 describe 'Garage' do
 
+  let(:bike) {Bike.new}
+  let(:garage) {Garage.new}
+
   it "should accept a bike" do
-    bike = Bike.new
-    garage = Garage.new
     expect(garage.bike_count).to eq(0)
     garage.accept_bike(bike)
     expect(garage.bike_count).to eq(1) 
   end
 
   it "should release a bike" do
-    bike = Bike.new
-    garage = Garage.new
     garage.accept_bike(bike)
     garage.release_bike(bike)
     expect(garage.bike_count).to eq(0)
   end
 
   it "should know when it is full" do
-    bike = Bike.new
-    garage = Garage.new
     expect(garage).not_to be_full
     50.times { garage.accept_bike(bike) }
     expect(garage).to be_full
   end
+
+  it "should not accept a bike when it is full" do
+  end
+
 
 
 
