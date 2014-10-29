c396155508491e4c01c728a814b4cd3cc6724c5d
Nick Dyer <nbdyer@gmail.com>
2014-10-29 19:56:50 +0000

    Improve fix a bike test

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -2,17 +2,16 @@ require 'garage'
 
 describe 'Garage' do
 
-  let(:bike) {Bike.new}
   let(:garage) {Garage.new(capacity: 200)}
 
-  it "should allow setting a default capacity on initializing" do
+  it "should allow setting of default capacity on initializing" do
     expect(garage.capacity).to eq(200)
   end
   
   it "should fix a bike" do
-    bike.break!
-    garage.repair!(bike)
-    expect(bike.broken?).to be false
+    broken_bike = double :bike, broken?: true
+    expect(broken_bike).to receive(:fix!)
+    garage.repair!(broken_bike)
   end
 
 
