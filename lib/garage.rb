b7777750ba15fdf90c7dd7167813c7bc23cc52a8
Nick Dyer <nbdyer@gmail.com>
2014-10-29 12:44:21 +0000

    Garage should know when it is full

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,6 +1,9 @@
 class Garage
 
-  def initialize
+DEFAULT_CAPACITY = 50
+
+  def initialize(options = {})
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
     @bikes = []
   end
 
@@ -14,6 +17,10 @@ class Garage
 
   def release_bike(bike)
     @bikes.delete(bike)
+  end
+
+  def full?
+    bike_count == @capacity
   end
 
 
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -2,7 +2,7 @@ require 'garage'
 
 describe 'Garage' do
 
-  it 'should accept a bike' do
+  it "should accept a bike" do
     bike = Bike.new
     garage = Garage.new
     expect(garage.bike_count).to eq(0)
@@ -10,13 +10,22 @@ describe 'Garage' do
     expect(garage.bike_count).to eq(1) 
   end
 
-  it 'should release a bike' do
+  it "should release a bike" do
     bike = Bike.new
     garage = Garage.new
     garage.accept_bike(bike)
     garage.release_bike(bike)
     expect(garage.bike_count).to eq(0)
   end
+
+  it "should know when it is full" do
+    bike = Bike.new
+    garage = Garage.new
+    expect(garage).not_to be_full
+    50.times { garage.accept_bike(bike) }
+    expect(garage).to be_full
+  end
+
 
 
 end
