7a5d4dfcc2b387e71b9e8052e007a48fdaa0f28d
Nick Dyer <nbdyer@gmail.com>
2014-10-30 19:43:41 +0000

    Docking station can transfer broken bikes to van

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,9 +5,17 @@ describe DockingStation do
   it_behaves_like 'BikeContainer'
 
   let(:station) { DockingStation.new(:capacity => 123) }
+  let(:van) {double :van}
+  let(:broken_bike) {double :bike, broken?: true, :class => Bike}
 
   it "should allow setting of default capacity on initializing" do
     expect(station.capacity).to eq(123)
+  end
+
+  it "should transfer broken bikes to van" do
+    10.times{station.dock(broken_bike)}
+    10.times{expect(van).to receive(:dock).with(broken_bike)}
+    station.transfer_bikes_to(van, station.available_broken_bikes)
   end
 
 end
