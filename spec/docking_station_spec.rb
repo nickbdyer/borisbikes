402b25808b7a4a70be16ab08d0fc74a414b1caae
Nick Dyer <nbdyer@gmail.com>
2014-10-30 19:54:04 +0000

    Add potential integration test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,13 @@
 require 'docking_station'
+require 'van'
 
 describe DockingStation do
 
   it_behaves_like 'BikeContainer'
 
   let(:station) { DockingStation.new(:capacity => 123) }
-  let(:van) {double :van}
+  let(:van) { double :van }
+  # let(:van) { Van.new }
   let(:broken_bike) {double :bike, broken?: true, :class => Bike}
 
   it "should allow setting of default capacity on initializing" do
@@ -17,6 +19,11 @@ describe DockingStation do
     10.times{expect(van).to receive(:dock).with(broken_bike)}
     station.transfer_bikes_to(van, station.available_broken_bikes)
   end
+
+  # it "should stop transferring bikes to van if van is full" do
+  #   30.times{station.dock(broken_bike)}
+  #   expect{(station.transfer_bikes_to(van, station.available_broken_bikes))}.to raise_error('There is no more space.')
+  # end
 
 end
 
